a40867dbb783cee1378f482b2ed53ec47a1050bb
Thomas Tai <thomastai@renaelectronics.com>
2017-08-03 20:32:29 -0400

    update
    Signed-off-by: Thomas Tai <thomastai@renaelectronics.com>

--- linuxcnc/src/emc/usr_intf/renaconf/build_package.sh
+++ linuxcnc/src/emc/usr_intf/renaconf/build_package.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 D=Debian/renaconf/files
+# glade file
+echo "copying glade files"
 cp *.glade ${D}/usr/share/linuxcnc/renaconf
+
+echo "copying py scripts"
 cp renaconf.py ${D}/usr/bin/renaconf
 chmod +x ${D}/usr/bin/renaconf
 cp *.py ${D}/usr/share/pyshared/renaconf/
 cd ./Debian/renaconf
+
+echo "build debian package: use sudo dpkg -i <packagename>"
 dpkg-buildpackage
