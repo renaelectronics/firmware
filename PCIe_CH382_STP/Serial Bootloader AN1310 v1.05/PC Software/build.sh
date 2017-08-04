acf515c8853b1130f436d2162177465d2de6aaa9
Thomas Tai <thomastai@renaelectronics.com>
2017-08-04 09:03:11 -0400

    add clean
    Signed-off-by: Thomas Tai <thomastai@renaelectronics.com>

--- PCIe_CH382_STP/Serial Bootloader AN1310 v1.05/PC Software/build.sh
+++ PCIe_CH382_STP/Serial Bootloader AN1310 v1.05/PC Software/build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 make clean
-rm ./AN1310cl/AN1310cl
-rm ./AN1310ui/AN1310ui
+rm -f ./AN1310cl/AN1310cl
+rm -f ./AN1310ui/AN1310ui
+rm -f ./AN1310ui/an1310ui_plugin_import.cpp
+rm -f `find -name moc*`
+rm -f `find -name *.a`
+
+# clean only
+if [ "$1" = "clean" ]; then
+	echo "clean only"
+	exit
+fi
+
+# qmake to generate Makefile then make
 for i in Bootload QextSerialPort AN1310ui AN1310cl
 do
 	cd $i
