96a822aa5e7cc06a73291b7f96312a7c2a918ce9
Thomas Tai <thomastai@renaelectronics.com>
2017-08-04 09:11:56 -0400

    add clean
    Signed-off-by: Thomas Tai <thomastai@renaelectronics.com>

--- linuxcnc/src/emc/usr_intf/renaconf/build.sh
+++ linuxcnc/src/emc/usr_intf/renaconf/build.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 D=Debian/renaconf/files
 echo "remove old package files"
-rm ./Debian/*.deb
+rm -f ./Debian/*.deb
+rm -f ./Debian/renaconf/debian/renaconf/usr/bin/AN1310cl
+rm -f ./Debian/renaconf/files/usr/bin/AN1310cl
+rm -f ./Debian/renaconf/debian/renaconf/DEBIAN/control
+rm -f ./Debian/renaconf/debian/renaconf/DEBIAN/md5sums
+rm -f ./Debian/renaconf/debian/renaconf/DEBIAN/postinst
+rm -r ./Debian/renaconf_1.0.0.dsc
+rm -f ./Debian/renaconf_1.0.0_i386.changes
+
+# clean only
+if [ "$1" = "clean" ]; then
+	echo "clean only"
+	exit
+fi
 
 echo "copying glade files"
 cp *.glade ${D}/usr/share/linuxcnc/renaconf
