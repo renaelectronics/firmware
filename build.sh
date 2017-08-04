001f2263209626cf83a31e23f2a4a625b7148c81
Thomas Tai <thomastai@renaelectronics.com>
2017-08-04 09:27:31 -0400

    update
    Signed-off-by: Thomas Tai <thomastai@renaelectronics.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 DIR=`pwd`
 echo "build AN1310cl ... "
 cd ~/rena/firmware/PCIe_CH382_STP/Serial\ Bootloader\ AN1310\ v1.05/PC\ Software/
-./build.sh
-cp ./AN1310cl/AN1310cl $DIR/linuxcnc/src/emc/usr_intf/renaconf/Debian/renaconf/files/usr/bin
+./build.sh $1
+if [ -e ./AN1310cl/AN1310cl ]; then
+	cp ./AN1310cl/AN1310cl $DIR/linuxcnc/src/emc/usr_intf/renaconf/Debian/renaconf/files/usr/bin
+fi
 cd $DIR/linuxcnc/src/emc/usr_intf/renaconf
-./build.sh
+./build.sh $1
--- linuxcnc/src/emc/usr_intf/renaconf/build.sh
+++ linuxcnc/src/emc/usr_intf/renaconf/build.sh
@@ -7,7 +7,7 @@ rm -f ./Debian/renaconf/files/usr/bin/AN1310cl
 rm -f ./Debian/renaconf/debian/renaconf/DEBIAN/control
 rm -f ./Debian/renaconf/debian/renaconf/DEBIAN/md5sums
 rm -f ./Debian/renaconf/debian/renaconf/DEBIAN/postinst
-rm -r ./Debian/renaconf_1.0.0.dsc
+rm -f ./Debian/renaconf_1.0.0.dsc
 rm -f ./Debian/renaconf_1.0.0_i386.changes
 
 # clean only
