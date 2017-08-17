76982d05eb91b0f4f5612d478c3d05310876a2cd
Thomas Tai <thomastai@renaelectronics.com>
2017-08-17 10:27:15 -0400

    update
    Signed-off-by: Thomas Tai <thomastai@renaelectronics.com>

--- wch6474/options.py
+++ wch6474/options.py
@@ -2,18 +2,27 @@
 import sys
 import argparse
 
+# constant
+OPT_SHORT=0
+OPT_LONG=1
+OPT_DEFAULT=2
+OPT_DESC=3
+OPT_MIN=4
+OPT_MAX=5
+OPT_UNIT=6
+
 # options which take string as argument
 string_options = [ \
-    ['-m', '--motor', 0, 'motor unit', 0, 3], \
-    ['-s', '--step_mode', 0, '0=full, 1=half, 2=1/4 step, 3=1/8 step, 4=1/16 step', 0, 4], \
-    ['-p', '--port', '/dev/partport0', 'parport port name, default is /dev/parport0', None, None], \
-    ['-c', '--current', 0.03125, 'drive current', 0.03125, 4.0], \
-    ['-w', '--pwm_off', 8.0, 'PWM off time, 4.0 to 124.0 (usec)', 4.0, 124.0], \
-    ['-t', '--t_fast', 4.0, 'fast decay time, 2.0 to 6.0 (usec)', 2.0, 32.0], \
-    ['-e', '--t_step', 4.0, 'fall step time, 2.0 to 32.0 (usec)', 2.0, 32.0], \
-    ['-o', '--ton_min',0.5, 'minimum on time, 0.5 to 64.0 (usec)', 0.5, 64.0], \
-    ['-f', '--toff_min',0.5, 'minimum off time, 0.5 to 64.0 (usec)', 0.5, 64.0], \
-    ['-d', '--ocd_th', 3.0, 'over current detection threshold', 0.375, 6.0], \
+    ['-m', '--motor', 0, 'motor unit', 0, 3,''], \
+    ['-s', '--step_mode', 0, '0=full, 1=half, 2=1/4 step, 3=1/8 step, 4=1/16 step', 0, 4, ''], \
+    ['-p', '--port', '/dev/partport0', 'parport port name, default is /dev/parport0', None, None, ''], \
+    ['-c', '--current', 0.03125, 'drive current', 0.03125, 4.0, 'A'], \
+    ['-w', '--pwm_off', 8.0, 'PWM off time, 4.0 to 124.0', 4.0, 124.0, 'us'], \
+    ['-t', '--t_fast', 4.0, 'fast decay time, 2.0 to 6.0', 2.0, 32.0, 'us'], \
+    ['-e', '--t_step', 20.0, 'fall step time, 2.0 to 32.0', 2.0, 32.0, 'us'], \
+    ['-o', '--ton_min',0.5, 'minimum on time, 0.5 to 64.0', 0.5, 64.0, 'us'], \
+    ['-f', '--toff_min',0.5, 'minimum off time, 0.5 to 64.0', 0.5, 64.0, 'us'], \
+    ['-d', '--ocd_th', 3.0, 'over current detection threshold', 0.375, 6.0, 'A'], \
 ]
 
 # boolean argument
@@ -31,7 +40,7 @@ def agrument_to_value(string, min, max):
     try:    
         if type(string) == type(""):
             # string value
-            if type(min) == None:
+            if type(min) == type(None):
                 return string 
 
             # integer value
@@ -42,37 +51,52 @@ def agrument_to_value(string, min, max):
             if type(min) == type(0.0):
                 rc = float(string)
 
+            # check range
+            if (rc < min) or (rc > max):
+                return None
+
     except ValueError:
-        print "failed to convert string"
         rc = None
 
     return rc
-        
-            
 
-
+# main        
 parser = argparse.ArgumentParser(description='Rena Motor Driver Configuration Program')
 
-# string agrument
+# add string agrument
 for option in string_options:
-    parser.add_argument(option[0], option[1], default=option[2], help=option[3])
+    parser.add_argument(option[OPT_SHORT], option[OPT_LONG], \
+                        default=option[OPT_DEFAULT], help=option[OPT_DESC])
 
-# boolean agrument
+# add boolean agrument
 for option in bool_options:
-    parser.add_argument(option[0], option[1], default=False, help=option[3], action='store_true')
+    parser.add_argument(option[OPT_SHORT], option[OPT_LONG], default=False, \
+                        help=option[OPT_DESC], action='store_true')
 
-# get the args
+# parser agrument
 args = parser.parse_args()
 
 # convert args to value
 for option in string_options:
     # attr name, min and max value
-    attr_name = option[1].lstrip('-')
-    min = option[4]
-    max = option[5]
-    attr = agrument_to_value(getattr(args, attr_name), min, max)
-    setattr(args, attr_name, attr)
-    print attr_name, type(attr), attr    
+    attr_name = option[OPT_LONG].lstrip('-')
+    min = option[OPT_MIN]
+    max = option[OPT_MAX]
+    unit = option[OPT_UNIT]
+    # value = args.attr_name
+    string_value = getattr(args, attr_name)
+    # convert string_value to string/int/float base on the type of min
+    value = agrument_to_value(string_value, min, max)
+    if (value == None):
+        # show min and max value
+        if (min != None):
+            print 'Invalid agrument \'{} {}\', {} must be between {} and {}'.format( \
+                    option[OPT_LONG], string_value, option[OPT_LONG], min, max)
+            break
+    setattr(args, attr_name, value)
+    print '{:10} {:16} {}{}'.format(attr_name, type(value), value, unit)
+
+
 
 
 #    print string
